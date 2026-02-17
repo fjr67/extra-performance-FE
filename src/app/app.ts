bf88de3a463776a175fb6683221ae36546dfd2ff
Finn <finnjrankin555@gmail.com>
2026-02-17 19:18:06 +0000

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('extra-performance-FE');
+  protected readonly title = signal('extra-performance-FE woohoo');
 }
