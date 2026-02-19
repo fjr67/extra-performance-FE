1f22c8717201427eda467db3b981fb2beb912cc9
Finn <finnjrankin555@gmail.com>
2026-02-19 16:06:11 +0000

    added prod environment config to allow for hashed routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('extra-performance-FE woohoo');
+  private baseUrl = environment.apiUrl;
+  protected readonly title = signal('extra-performance-FE '+this.baseUrl);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig as appConfigProd } from './app/app.config.prod';
+import { environment } from './environments/environment';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, environment.production ? appConfigProd : appConfig)
   .catch((err) => console.error(err));
